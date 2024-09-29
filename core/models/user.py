40c9e928e9dd7aae06919bbd04b524c2c8767770
Roman <cheepa30@gmail.com>
2024-09-30 00:38:02 +0300

    added base & user models

---
+++ core/models/user.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from . import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
