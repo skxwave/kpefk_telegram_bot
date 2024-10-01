a1ec004a5f7c03d2ac359395a2697e53f9fd43ad
Roman <cheepa30@gmail.com>
2024-10-01 14:54:43 +0300

    added user creation to database

--- bot/handlers/commands.py
+++ bot/handlers/commands.py
@@ -3,12 +3,19 @@ from aiogram.types import Message
 from aiogram.filters.command import Command
 
 from bot.keyboards import get_menu
+from core import db_helper
 
 router = Router()
 
 
 @router.message(Command("start", "menu"))
 async def start_message(message: Message):
+    user = await db_helper.find_user(int(message.from_user.id))
+    if not user:
+        await db_helper.create_user(
+            username=message.from_user.username,
+            telegram_id=message.from_user.id,
+        )
     await message.answer(
         "*Menu:*",
         reply_markup=get_menu(),
