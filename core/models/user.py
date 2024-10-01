84112706f1eab30212d7d69278018eccdee34de7
Roman <cheepa30@gmail.com>
2024-10-01 14:53:31 +0300

    add new columns to users

--- core/models/user.py
+++ core/models/user.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BOOLEAN
 
 from . import Base
 
@@ -9,3 +10,5 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str]
     telegram_id: Mapped[int] = mapped_column(nullable=False)
+    group: Mapped[int] = mapped_column(nullable=True)
+    is_admin: Mapped[bool] = mapped_column(default="False", server_default="False")
