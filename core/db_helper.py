c38f70fef77460f2f0b81f00b9fb8ce818e9ebe7
Roman <cheepa30@gmail.com>
2024-10-01 16:03:19 +0300

    created new CRUD operations

--- core/db_helper.py
+++ core/db_helper.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import (
 from sqlalchemy import select
 
 from .config import settings
-from core.models import User
+from core.models import User, Schedule
 
 
 class DatabaseHelper:
@@ -52,3 +52,44 @@ async def find_user(telegram_id: int):
     async with db_helper.session_factory() as session:
         user = await session.scalar(select(User).where(User.telegram_id == telegram_id))
     return user
+
+
+async def set_group(telegram_id: int, group: int):
+    async with db_helper.session_factory() as session:
+        user = await session.scalar(select(User).where(User.telegram_id == telegram_id))
+        user.group = group
+        await session.commit()
+    return user
+
+
+async def add_lesson(**kwargs):
+    """
+    Add a lesson to the schedule.
+
+    Args:
+        day (str): The day of the week (e.g., "Monday").
+        group (int): The group number (e.g., 101).
+        lesson_number (int): The order number of the lesson (e.g., 1, 2, 3...).
+        subject (str): The subject of the lesson (e.g., "Math").
+        classroom (str): The room where the lesson takes place (e.g., "101").
+        teacher (str): The teacher's name (e.g., "Smith").
+        numerator_denominator (str): Indicates if it's numerator or denominator (e.g., "numerator").
+    """
+    async with db_helper.session_factory() as session:
+        existing_lesson = await session.scalars(select(Schedule).filter_by(**kwargs))
+        if existing_lesson.first():
+            return "Lesson exists"
+        else:
+            lesson = Schedule(**kwargs)
+            session.add(lesson)
+            await session.commit()
+    return lesson
+
+
+async def find_lessons(group: int, day: str):
+    async with db_helper.session_factory() as session:
+        lessons = await session.scalars(
+            select(Schedule).filter(Schedule.group == group, Schedule.day == day)
+        )
+
+    return lessons
