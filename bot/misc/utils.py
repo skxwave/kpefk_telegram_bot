0c16d0e07f747abb35fb05c239a5319c2b828119
Roman <cheepa30@gmail.com>
2024-10-14 17:10:20 +0300

    created send_user_info func

--- bot/misc/utils.py
+++ bot/misc/utils.py
@@ -1,6 +1,14 @@
 import requests
 
+from core import db_helper
+
 
 async def group_exist(group: str | int) -> bool:
     groups = requests.get(f"https://skxwave.pythonanywhere.com/api/v1/groups/")
     return True if group in groups.json()["result"] else False
+
+
+async def send_user_info(telegram_id: int):
+    user = await db_helper.find_user(telegram_id)
+    result = f"Вибрана група: `{user.group}`"
+    return result
